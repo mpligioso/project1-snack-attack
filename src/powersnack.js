69e1f4869e1d9640ab37093d24b4dcf5b1ffcebb
Meri-Pier Ligioso <mpligioso@gmail.com>
2018-06-15 11:04:07 +0200

    adding sounds

--- src/powersnack.js
+++ src/powersnack.js
@@ -2,6 +2,75 @@
 //-----------
 window.onload = function () {
 
+// function Animation(frameSet, delay){
+//   this.count = 0;
+//   this.delay = delay;
+//   this.frame = 0;
+//   this.frameIndex = 0;
+//   this.frameSet = frameSet
+// }
+
+// Animation.prototype.change = function (frameSet, delay=15){
+//   if(this.frameSet != frameSet){
+//     this.count = 0;
+//     this.delay = delay;
+//     this.frameIndex = 0;
+//     this.frameSet = frameSet,
+//     this.frame = this.frameSet[this.frameIndex]
+//   }
+// }
+
+// Animation.prototype.update = function(){
+//   this.count ++;
+//   if (this.count >= this.delay){
+//     this.count = 0;
+
+//     this.frameIndex = (this.frameIndex == this.frameSet.length-1) ? 0: this.frameIndex + 1;
+//     this.frame = this.frameSet[this.frameIndex];
+//   }
+// }
+
+// spriteSheet = {
+//   frameSets: [[0,1,2,3,4,],[5,6,7,8,9]], //to right, to left
+//   image: new Image()
+// };
+
+// spriteSheet.image.src = "./images/pacman-spritesheet-movements.png"
+
+function Sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("perload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+// Sound.prototype.fadeOutAudio = function (){
+//     if(status != 'play') {
+//       return false;
+//     }
+
+//     var actualVolume = this.sound.volume;
+//     var fadeOutInterval = setInterval(function(){
+//         actualVolume = (parseFloat(actualVolume) - 0.1).toFixed(1);
+//         if(actualVolume >= 0){
+//             this.sound.volume = actualVolume;
+//         } else {
+//             this.sound.pause();
+//             status = 'pause';
+//             clearInterval(fadeOutInterval);
+//         }
+//     }, 50);
+// }
+
+// function Player (myImage){
 function Player (myImage){
   this.image = myImage;
   this.width = 50;
@@ -10,15 +79,70 @@ function Player (myImage){
   this.x = 400;
   this.y = 300;
 
-  this.speed = 0;
+  this.speed = 1;
   this.points = 0;
+  // this.direction = undefined;
+
+  // animation = new Animation();
 }
+
+// loop = function () {
+//   if (newPlayer.direction === "right"){
+//     newPlayer.animation.change(spriteSheet.frameSet[0], 15);
+//   }
+
+//   if (newPlayer.direction === "left"){
+//     newPlayer.animation.change(spriteSheet.frameSet[1], 15);
+//   }
+
+//   render();
+//   window.requestAnimationFrame(loop);
+// }
+
+// render = function(){
+//   ctx.drawImage(sprite_sheet.image, player.animation.frame * SPRITE_SIZE, 0, SPRITE_SIZE, SPRITE_SIZE, Math.floor(player.x), Math.floor(player.y), SPRITE_SIZE, SPRITE_SIZE);
+// }
+
+
 
 Player.prototype.draw = function(){
   ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
-  // ctx.fill();
-  // ctx.closePath();
 }
+// Player.prototype.render = function () {
+
+//   ctx.clearRect(0, 0, this.width, this.height);
+
+//   ctx.drawImage(
+//   this.image,
+//   0,
+//   frameIndex * this.width / numberOfFrames,
+//   0,
+//   this.width / numberOfFrames,
+//   this.height,
+//   0,
+//   0,
+//   this.width / numberOfFrames,
+//   this.height);
+// };
+
+// Player.prototype.update = function () {
+//       this.tickCount += 1;
+//       if (this.tickCount > this.ticksPerFrame) {
+//         tickCount = 0;
+//           // Go to the next frame
+//           if (frameIndex < numberOfFrames - 1) {
+//             // Go to the next framethat
+//             frameIndex += 1;
+//           } else {
+//             frameIndex = 0;
+//           }
+//         };
+// }
+
+// function gameLoop (player) {
+//   player.update();
+//   player.render();
+// }
 
 Player.prototype.move = function(){
   document.onkeydown = (e) => {
@@ -26,28 +150,28 @@ Player.prototype.move = function(){
       case 38:
         if (this.y - 10 > 10){
         this.y -= 10;
-        console.log("up")
+        this.direction = "up";
         };
         e.preventDefault()
         break;
       case 40:
         if (this.y + 10 < canvas.height-10){
         this.y += 10;
-        console.log("down")
+        this.direction = "down";
         };
         e.preventDefault()
         break;
       case 37:
         if (this.x - 10 > 10){
         this.x -= 10;
-        console.log("left")
+        this.direction = "left";
         };
         e.preventDefault()
         break;
       case 39:
         if (this.x + 10 < canvas.width-10) {
         this.x += 10;
-        console.log("right")
+        this.direction = "right";
         };
         e.preventDefault()
         break;
@@ -61,7 +185,9 @@ Player.prototype.receivePoints = function (snack) {
 
 //snack model
 function Snack(myImage, width, height) {
-  Player.call(this, myImage, width, height);
+  this.image = myImage;
+  this.width = width;
+  this.height = height;
 
   this.x = Math.floor(Math.random()* (canvas.width-50));
   this.y = Math.floor(Math.random()* (canvas.height-50));
@@ -70,12 +196,22 @@ function Snack(myImage, width, height) {
 
   this.isGood = true;
 
+  this.hasSpeed = false;
+
   this.points = 5;
 
   // this.density = 5;
 }
 
-Snack.prototype = Object.create(Player.prototype)
+Snack.prototype.draw = function(){
+  ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+  if (this.hasMovement){
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+}
+
+// Snack.prototype = Object.create(Player.prototype)
 
 function SuperSnack (myImage, width, height){
   Snack.call(this, myImage, width, height);
@@ -86,15 +222,18 @@ function SuperSnack (myImage, width, height){
   // this.density = 0;
 }
 
+SuperSnack.prototype = Object.create(Snack.prototype)
 
 function MovingSnack (myImage, width, height){
   Snack.call(this, myImage, width, height);
+  this.x = canvas.width;
   this.vx = Math.floor(Math.random()* 10);
   this.vy = Math.floor(Math.random()* 10);
+  this.hasMovement = true;
   this.points = 5;
 }
 
-SuperSnack.prototype = Object.create(Snack.prototype)
+MovingSnack.prototype = Object.create(Snack.prototype)
 
 //CANVAS
 //---------------
@@ -102,36 +241,66 @@ SuperSnack.prototype = Object.create(Snack.prototype)
 var canvas = document.getElementById('snackboard');
 
 var ctx = canvas.getContext('2d');
-var timer = 60;
+var timer = 10;
 
 // Images
 var playerImg = new Image();
-playerImg.src = "./images/Cherry.png"
+playerImg.src = "./images/pacman-spritesheet.png"
+
+var friesImg = new Image();
+friesImg.src = "./images/fries.png"
+
+var bigMacImg = new Image();
+bigMacImg.src = "./images/bigmac.png"
+
+var pizzaImg = new Image();
+pizzaImg.src = "./images/pizza.png"
+
+
 
 var beerImg = new Image();
 beerImg.src = "./images/Beer.png"
 
-var baconImg = new Image();
-baconImg.src = "./images/Bacon.png"
+var donutImg = new Image();
+donutImg.src = "./images/donut.png"
 
-var cheeseImg = new Image();
-cheeseImg.src = "./images/Cheese.png"
+var colaImg = new Image();
+colaImg.src = "./images/cola.png"
 
-var avoImg = new Image();
-avoImg.src = "./images/Avocado.png"
+var lemonlimeImg = new Image();
+lemonlimeImg.src = "./images/cola.png"
 
-var appleImg = new Image();
-appleImg.src = "./images/Apple.png"
+
+
+var tomatoImg = new Image();
+tomatoImg.src = "./images/Tomato.png"
+
+var turnipImg = new Image();
+turnipImg.src = "./images/Turnip.png"
 
 var eggplantImg = new Image();
 eggplantImg.src = "./images/Eggplant.png"
 
-var eggsImg = new Image();
-eggsImg.src = "./images/Eggs.png"
+var onionImg = new Image();
+onionImg.src = "./images/Onion.png"
 
-var newPlayer = new Player(playerImg, 50, 50);
 
-var snackImages = [baconImg, cheeseImg, avoImg, appleImg, eggplantImg, eggsImg];
+var winningMusic = new Sound("./sounds/applause3.mp3");
+var losingMusic = new Sound("./sounds/fail.wav")
+var backgroundMusic = new Sound("./sounds/background-music.mp3")
+var badFood = new Sound("./sounds/badfood.wav")
+var superBonus = new Sound("./sounds/Bonus.wav")
+
+
+var newPlayer = new Player(playerImg);
+
+var snackImages = [eggplantImg,
+  onionImg,
+  turnipImg,
+  tomatoImg,
+  bigMacImg,
+  pizzaImg,
+  colaImg];
 
 var fixedSnacks = [];
 
@@ -146,18 +315,30 @@ function checkCollision(objA, objB){
 
 var isOver = false;
 
+$('.game-over').hide();
+
 function isGameOver(){
   if (timer < 0){
     isOver = true;
-    $($gameOverDiv).hide().appendTo(".layout").fadeIn(500);
+
+    $('.game-over').fadeIn(500);
 
     if (newPlayer.points > 100) {
-      $(".game-over").append($winner);
+      $("#winner").show();
+      winningMusic.play();
     } else {
-      $(".game-over").append($loser);
+      $("#loser").show();
+      losingMusic.play()
     }
+
+    $('.try-btn').click(function() {
+      $('.game-over').hide();
+      runGame();
+    });
   }
 };
+
+
 
 function startCountdown() {
   timerCountdown =
@@ -178,33 +359,10 @@ function initSuperSnacks(){
     console.log(stableSnacks)
   };
 
-  // stableSnacks = stableSnacks.filter((item) => {
-  //   return Math.abs(newPlayer.x - item.x) > 50 || Math.abs(newPlayer.y - item.y) > 50;
-  // });
+  stableSnacks = stableSnacks.filter((item) => {
+    return Math.abs(newPlayer.x - item.x) > 50 || Math.abs(newPlayer.y - item.y) > 50;
+  });
 }
-
-// function initSnacks(snackArray) {
-//   snackArray.forEach((oneSnack) => {S
-//     var isCollision = checkCollision(newPlayer,oneSnack);
-//     if (isCollision){
-//       if(oneSnack.isGood){
-//         oneSnack.isEaten = true;
-//         newPlayer.receivePoints(oneSnack)
-//       } else {
-//         oneSnack.isEaten = true;
-//         newPlayer.receivePoints(oneSnack)
-//       }
-//     }
-//     oneSnack.draw();
-//   });
-// };
-
-// function removeSnacks(snackArray){
-//   snackArray = snackArray.filter(function(oneSnack){
-//     return !oneSnack.isEaten;
-//   });
-// }
-
 
 function addingSnacks() {
   addSnacks =
@@ -214,8 +372,8 @@ function addingSnacks() {
     var height = 50;
 
     var newSnack = new Snack(fixedImage, 50, height);
-    if (fixedImage === appleImg || fixedImage === eggplantImg ||
-      fixedImage === eggsImg){
+    if (fixedImage === tomatoImg || fixedImage === eggplantImg ||
+      fixedImage === onionImg || fixedImage === turnipImg){
       newSnack.isGood = false;
       newSnack.points *= -1;
     }
@@ -225,95 +383,130 @@ function addingSnacks() {
   }, 800)
 };
 
+
+function addingMovingSnacks() {
+  addMovingSnacks =
+  setInterval(() => {
+    var randomNumber = Math.floor(Math.random() * snackImages.length)
+    var fixedImage = snackImages[randomNumber];
+    var height = 50;
+
+    var newSnack = new MovingSnack(fixedImage, 50, height);
+    if ((fixedImage === tomatoImg) || (fixedImage === eggplantImg) ||
+      (fixedImage === onionImg) || (fixedImage === turnipImg)){
+      newSnack.isGood = false;
+      newSnack.points *= -1;
+    }
+    if(movingSnacks.length < 7){
+      movingSnacks.push(newSnack);
+    };
+  }, 10000)
+};
+
+// function updateSnackMovement(){
+//   isMoving =
+//   setInterval(() => {
+//     ctx.clearRect(0,0, canvas.width, canvas.height);
+//     movingSnacks.forEach(function(oneSnack){
+//       oneSnack.draw();
+//       oneSnack.x += oneSnack.vx;
+//       oneSnack.y += oneSnack.vy;
+//     })
+//   },20)
+// }
+
+function updateSnacks(snackArray) {
+  snackArray.forEach((oneSnack) => {
+    var isCollision = checkCollision(newPlayer,oneSnack);
+    if (isCollision){
+      oneSnack.isEaten = true;
+      newPlayer.receivePoints(oneSnack)
+      if (!oneSnack.isGood){
+        badFood.play();
+      } else if (oneSnack.isSuper){
+        superBonus.play();
+      }
+    }
+    oneSnack.draw();
+  });
+
+  return snackArray.filter(function(oneSnack){
+    return !oneSnack.isEaten;
+  });
+};
+
+
 function initGame () {
+  if(!isOver){
+    backgroundMusic.play();
+  }
   drawGame =
   setInterval(() => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     newPlayer.draw();
     newPlayer.move();
 
     $('.points span').text(newPlayer.points);
 
+    stableSnacks = updateSnacks(stableSnacks);
+    fixedSnacks = updateSnacks(fixedSnacks);
+    movingSnacks = updateSnacks(movingSnacks);
 
-
-    stableSnacks.forEach((oneSnack) => {
-      var isCollision = checkCollision(newPlayer,oneSnack);
-      if (isCollision){
-        if(oneSnack.isGood){
-          oneSnack.isEaten = true;
-          newPlayer.receivePoints(oneSnack)
-        } else {
-          oneSnack.isEaten = true;
-          newPlayer.receivePoints(oneSnack)
-        }
-      }
-      oneSnack.draw();
-    });
-
-    stableSnacks = stableSnacks.filter(function(oneSnack){
-      return !oneSnack.isEaten;
-    });
-
-
-    fixedSnacks.forEach((oneSnack) => {
-      var isCollision = checkCollision(newPlayer,oneSnack);
-      if (isCollision){
-        if(oneSnack.isGood){
-          oneSnack.isEaten = true;
-          newPlayer.receivePoints(oneSnack)
-        } else {
-          oneSnack.isEaten = true;
-          newPlayer.receivePoints(oneSnack)
-        }
-      }
-      oneSnack.draw();
-    });
-
-    fixedSnacks = fixedSnacks.filter(function(oneSnack){
-      return !oneSnack.isEaten;
-    });
 
     isGameOver();
     if(isOver){
       clearInterval(drawGame);
       clearInterval(addSnacks);
       clearInterval(timerCountdown);
+      backgroundMusic.stop()
+      ctx.clearRect(0,0,canvas.width,canvas.height)
     }
-
   }, 1000/60);
+};
+
+function resetGame(){
+  isOver = false;
+  timer = 10;
+  stableSnacks = []
+  fixedSnacks = []
+  newPlayer.points = 0;
+}
+
+function runGame() {
+  startCountdown();
+  initSuperSnacks();
+  addingSnacks();
+  addingMovingSnacks();
+  initGame();
+  resetGame();
 };
 
 
 // DOM && Other
 //----------------------------
 
-var $gameOverDiv =
-("<div class='game-over'></div>");
+// var $gameOverDiv =
+// ("<div class='game-over'></div>");
 
-var $winner =
-("<img src='./images/win.gif'> <button class='try-btn btn'>PLAY AGAIN</button>");
+// var $winner =
+// ("<img src='./images/win.gif'><br><button class='try-btn btn'>PLAY AGAIN</button>");
 
-var $loser =
-("<img src='./images/lose.gif'><button class='try-btn btn'>TRY AGAIN</button>")
+// var $loser =
+// ("<img src='./images/lose.gif'><br><button class='try-btn btn'>TRY AGAIN</button>")
 
+// $(document).ready(function() {
 
   $('.start-btn').click(() => {
     $('.start-page').fadeOut('2');
-    startCountdown();
-    initSuperSnacks();
-    addingSnacks();
-    initGame()
+    runGame();
+
   });
 
 
-  // $('.try-btn').click(() => {
-  //   document.reload();
-  //   $('.game-over').toggle();
-  //   startCountdown();
-  //   initSuperSnacks();
-  //   addingSnacks();
-  //   initGame()
-  // });
+
+
+// });
 
   // $($gameOverDiv).hide().appendTo(".layout").fadeIn(500);
 
